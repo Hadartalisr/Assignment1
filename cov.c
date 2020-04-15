d4350198b55a59687ac1ae0824d3f394f888d1ff
Roye <tadmoroye@gmail.com>
2020-04-15 17:41:57 +0300

    Added function for calculating cov matrix

--- cov.c
+++ cov.c
@@ -32,7 +32,9 @@ int main(int argc, char* argv[]) {
 	int numberOfColumns = 0; /* 1st argument in the file */
 	int numberOfRows = 0; /* 2nd argument in the file */
 	int ass = 0; /* For assertions */
-    double* matrix; /* Covariance matrix */
+    double* matrix; /* Input matrix */
+    double* covMatrix; /* Covariance matrix */
+
 
 
 
@@ -64,9 +66,9 @@ int main(int argc, char* argv[]) {
     ass = fread(matrix, sizeof(double),numberOfColumns*numberOfRows,file);
     assert(ass==numberOfColumns*numberOfRows);
     PrintMat(&matrix[0],numberOfRows,numberOfColumns);
-    CalculateVectorWithMeanSub(&matrix[0],6,numberOfRows,numberOfColumns);
-    CalculateDotProduct(&matrix[0],6,6,numberOfColumns);
-    PrintMat(&matrix[0],numberOfRows,numberOfColumns);
+    covMatrix = CalculateCovMatrix(&matrix[0],numberOfRows,numberOfColumns);
+    PrintMat(&covMatrix[0],numberOfRows,numberOfRows);
+
 
 
 
@@ -84,6 +86,8 @@ int main(int argc, char* argv[]) {
 
 int PrintMat(double* matrix,int numberOfRows,int numberOfColumns)
 {
+    /* Declarations */
+
     int i,j; /* Iterators */
 
     for (i = 0; i < numberOfRows; ++i) {
@@ -115,10 +119,27 @@ double CalculateMeanForSingleVector(double* matrix, int vector, int numberOfRows
 }
 int CalculateVectorWithMeanSub(double* matrix, int vector, int numberOfRows, int numberOfColumns)
 {
-    int i;
-    double mean = CalculateMeanForSingleVector(&matrix[0],vector,numberOfRows,numberOfColumns);
+    /* Receives a vector and subtracts its mean from each cell */
+
+    /* Declarations */
+    int i;     /* Iterator */
+    double mean; /* Mean of a given vector */
+
+    mean = CalculateMeanForSingleVector(&matrix[0],vector,numberOfRows,numberOfColumns);
     for (i = 0; i < numberOfColumns; ++i) {
         matrix[vector*numberOfColumns+i] -= mean;
+    }
+    return EXIT_SUCCESS;
+}
+int CalculateMatrixWithMeanSub(double* matrix, int numberOfRows,int numberOfColumns)
+{
+    /* Subtract the mean of each vector in the matrix from itself */
+
+    /* Declarations */
+    int i;  /* Iterator */
+
+    for (i = 0; i < numberOfRows; ++i) {
+        CalculateVectorWithMeanSub(matrix,i,numberOfRows,numberOfColumns);
     }
     return EXIT_SUCCESS;
 }
@@ -127,9 +148,27 @@ double CalculateDotProduct(double* matrix, int vector1, int vector2, int numberO
     double product;
     int i;
     product = 0;
+    printf("Calculating dot product between vector %d and %d\n",vector1,vector2);
     for (i = 0; i < numberOfColumns; ++i) {
         product += matrix[numberOfColumns*vector1 +i] * matrix[numberOfColumns*vector2 + i];
     }
-    printf("%f\n",sqrt(product));
-    return sqrt(product);
+    printf("%f\n",product);
+    return product;
+}
+double* CalculateCovMatrix(double* matrix, int numberOfRows, int numberOfColumns)
+{
+    double * covMatrix;
+    double product;
+    int i;
+    int j;
+    covMatrix = (double*)malloc(numberOfRows*numberOfRows* sizeof(double));
+    CalculateMatrixWithMeanSub(matrix,numberOfRows,numberOfColumns);
+    for (i = 0; i < numberOfRows; ++i) {
+        for ( j = 0; j < numberOfRows; ++j) {
+            product = CalculateDotProduct(matrix,i,j,numberOfColumns);
+            covMatrix[i*numberOfRows + j] = product;
+        }
+    }
+    PrintMat(covMatrix,numberOfRows,numberOfRows);
+    return covMatrix;
 }
--- cov.h
+++ cov.h
@@ -8,4 +8,6 @@ int PrintMat(double* matrix,int numberOfRows,int numberOfColumns);
 double CalculateMeanForSingleVector(double* matrix, int vector, int numberOfRows, int numberOfColumns);
 int CalculateVectorWithMeanSub(double* matrix, int vector, int numberOfRows, int numberOfColumns);
 double CalculateDotProduct(double* matrix, int vector1, int vector2, int numberOfColumns);
+int CalculateMatrixWithMeanSub(double* matrix, int numberOfRows,int numberOfColumns);
+double* CalculateCovMatrix(double* matrix, int numberOfRows, int numberOfColumns);
 #endif /*ASSIGNMENT1_COV_H*/
